e05a1641952463202edb10d0686591f40dd5d57c
renzynx <10262118-renzynx@users.noreply.gitlab.com>
2022-03-10 14:39:42 +0700

    bad idea

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import NProgress from "nprogress";
-import Router from "next/router";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 
 NProgress.configure({ showSpinner: false });
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
